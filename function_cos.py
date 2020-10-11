a0e4f6c8832456c0296fb74e2eb5a3842dbe816a
gordei513 <71318696+gordei513@users.noreply.github.com>
2020-10-11 17:35:49 +0300

    Update function_cos.py

--- function_cos.py
+++ function_cos.py
@@ -1,5 +1,6 @@
 import math
 
+
 ITERATIONS = 20
 
 def my_cos(x):
@@ -13,9 +14,22 @@ def my_cos(x):
     for n in range(1, ITERATIONS):
         x_pow *= x**2  # Каждый слеющий член больше в X^2 Раз + числовой коэфициент
         multiplier *= -1 / (2*n-1) / (2*n )  # тут уже следующий коэфициент(числовой): f(n+1)= f((n) * x^2 /(2*(n+1)-1) / (2*(n+1))
-        partial_sum += x_pow * multiplier
+        partial_sum += x_pow * multiplier #тут чисто складываются
     
     return partial_sum
 
-print(help(math.cos), math.cos(2))
-print(help(my_cos), my_cos(2))
+print(help(math.cos), math.cos(1.2))
+print(help(my_cos), my_cos(1.2))
+
+
+
+import math
+import cmath
+
+complex_angle = cmath.acos(5)
+print('"Угол", при котором косинус равен 4:', complex_angle)
+
+print("Косинус такого угла через эту функцию: ", my_cos(complex_angle))
+print("Косинус такого угла через библиотечную: ", cmath.cos(complex_angle))
+
+
