33ab63b8cfa28c94dcb2c69c51ba40988ed13c98
gordei513 <71318696+gordei513@users.noreply.github.com>
2020-10-11 17:22:55 +0300

    Update function_cos.py

--- function_cos.py
+++ function_cos.py
@@ -11,8 +11,8 @@ def my_cos(x):
     multiplier = 1
     partial_sum = 1
     for n in range(1, ITERATIONS):
-        x_pow *= x**2  # Каждый слеющий член больше в X^2 Раз + остальное учтено далее
-        multiplier *= -1 / (2*n-1) / (2*n )  # (-1)^n и факториал
+        x_pow *= x**2  # Каждый слеющий член больше в X^2 Раз + числовой коэфициент
+        multiplier *= -1 / (2*n-1) / (2*n )  # тут уже следующий коэфициент(числовой): f(n+1)= f((n) * x^2 /(2*(n+1)-1) / (2*(n+1))
         partial_sum += x_pow * multiplier
     
     return partial_sum
